7842c10b60a1c5e0ef0e9221a5db7f0636c74c89
Amir <amirni98@gmail.com>
2021-11-09 00:31:03 +0330

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = () => {
+            video.play();
+        }
+
+    } catch (error) {
+        
+    }
+}
+
+button.addEventListener('click' , async () => {
+    button.disabled = true;
+    await video.requestPictureInPicture();
+    button.disabled = false;
+});
+
+selectMediaStream();
